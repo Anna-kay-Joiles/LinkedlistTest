709c99b1f32a9104554a698bfcad45259437c978
annak <annak@ANNIE>
2024-03-28 17:58:36 -0600

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LinkedlistTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("LinkedlistTest")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d2c79085-e278-4a78-bf74-5631812108cc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace LinkedlistTest
+{
+    [TestClass]
+    public class LinkedListTests
+    {
+        [TestMethod]
+        public void Test_AddFirst()
+        {
+            LinkedList list = new LinkedList();
+            string expectedValue = "Joe Blow";
+            list.AddFirst(expectedValue);
+            Assert.AreEqual(expectedValue, list.GetValue(0));
+        }
+
+        [TestMethod]
+        public void Test_AddLast()
+        {
+            LinkedList list = new LinkedList();
+            string expectedValue = "Joe Schmoe";
+            list.AddLast(expectedValue);
+            Assert.AreEqual(expectedValue, list.GetValue(0));
+        }
+
+        [TestMethod]
+        public void Test_RemoveFirst()
+        {
+            LinkedList list = new LinkedList();
+            list.AddFirst("Joe Blow");
+            list.RemoveFirst();
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [TestMethod]
+        public void Test_RemoveLast()
+        {
+            LinkedList list = new LinkedList();
+            list.AddLast("Joe Blow");
+            list.RemoveLast();
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [TestMethod]
+        public void Test_GetValue()
+        {
+            LinkedList list = new LinkedList();
+            list.AddFirst("Joe Blow");
+            list.AddLast("Joe Schmoe");
+            list.AddLast("John Smith");
+            string expectedValue = "Joe Schmoe";
+            string retrievedValue = list.GetValue(1);
+            Assert.AreEqual(expectedValue, retrievedValue);
+        }
+    }
+}
